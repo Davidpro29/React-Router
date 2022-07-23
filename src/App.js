114bfc7ad2fe1d426d2297ede734e1575e94f819
David Robison <silva.estudo@hotmail.com>
2022-07-23 10:15:40 -0300

    configuracao do router

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+// config react router
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+
+// pages
+import Home from './pages/Home';
+import About from './pages/About';
+
+// components
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Router</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+
+import { Link } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+
+import './Home.css'
+
+export default function Home(){
+    return(
+        <div>
+            <h3>Home</h3>
+        </div>
+    )
+}
